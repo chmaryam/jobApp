a148b92d6233e7252721d7d262ff799a9395f8d5
maryam23 <emohammad273@gmail.com>
2023-02-27 12:30:14 +0300

    job edit,update on addjoboffer.ejs + viewjobejs /all team members

--- routes/job.js
+++ routes/job.js
@@ -7,6 +7,7 @@ const jobCntrl = require('../controllers/jobs')
 router.get('/job/addJobOffers', jobCntrl.job_create_get)
 router.post('/job/addJobOffers', jobCntrl.job_create_post)
 router.get("/job/viewJob", jobCntrl.job_view_get);
-// router.get("/job/viewJob", jobCntrl.job_show_get);
+router.get("/job/editJob", jobCntrl.job_edit_get);
+router.post("/job/editJob", jobCntrl.job_edit_post);
 module.exports= router
 
