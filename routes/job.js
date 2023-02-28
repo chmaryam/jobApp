9adae3780b12ff6d3b2d17bc235f6b9bd7adaa74
fatima20 <f6iim.ali@gmail.com>
2023-02-28 13:59:02 +0300

    adding view Job and edit auth route and controller

--- routes/job.js
+++ routes/job.js
@@ -6,11 +6,18 @@ const jobCntrl = require('../controllers/jobs')
 
 router.get("/job/addJobOffers", jobCntrl.job_create_get)
 router.post("/job/addJobOffers", jobCntrl.job_create_post)
+
 router.get("/job/viewJob", jobCntrl.job_view_get);
+
 router.get("/job/editJob", jobCntrl.job_edit_get);
 router.post("/job/editJob", jobCntrl.job_edit_post);
+
 router.get("/job/detail", jobCntrl.job_show_get)
 router.get("/job/delete", jobCntrl.job_delete_get);
-router.get("/applicant/applyjob", jobCntrl.applicant_create_get)
+// router.get("/applicant/applyjob", jobCntrl.applicant_create_get)
+
+router.get("/job/MyJobOffers",jobCntrl.job_viewJobOffer_get)
+// router.post("/job/myJobOffer",jobCntrl.job_view_post)
+
 module.exports= router
 
