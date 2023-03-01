57989ccfe84e3b25932106c43b9e536f8d507b63
maryam23 <emohammad273@gmail.com>
2023-03-01 09:55:31 +0300

    test

--- routes/applicant.js
+++ routes/applicant.js
@@ -4,7 +4,6 @@ const router = express.Router()
 const applicantControl = require('../controllers/applicant')
 
 
-router.get('/applicant/applyJob' ,applicantControl.applicant_applyJob_get )
-router.post('/applicant/applyJob' ,applicantControl.applicant_applyJob_post )
+router.get('/applicant/applyJob' ,applicantControl.applicant_create_get )
 
 module.exports = router
